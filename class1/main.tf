0457d9bc2c71f00d1f5ec474de26497c7c6fdf36
Shukur91 <ssean6754@gmail.com>
2025-01-24 11:23:30 -0600

    added a group

--- class1/main.tf
+++ class1/main.tf
@@ -1,6 +1,18 @@
+# Create IAM group
+resource "aws_iam_group" "devops_group" {
+  name = "devops-group"
+}
+
+
+
+
+
+
+
 resource "aws_iam_user" "bob" {
   name = "bob"
 }
 resource "aws_iam_user" "Kate" {
   name = "Kate"
 }
+
