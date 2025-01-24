da232bf2c1295a00f8d65c2e014686be116e1cd5
Shukur91 <ssean6754@gmail.com>
2025-01-24 11:26:39 -0600

    added users to the group

--- class1/main.tf
+++ class1/main.tf
@@ -3,7 +3,16 @@ resource "aws_iam_group" "devops_group" {
   name = "devops-group"
 }
 
+# Add 'bob' to the group
+resource "aws_iam_group_membership" "devops_group_membership" {
+  name  = "devops-group-membership"
+  group = aws_iam_group.devops_group.name
 
+  users = [
+    aws_iam_user.bob.name,
+    aws_iam_user.kate.name
+  ]
+}
 
 
 
@@ -12,7 +21,7 @@ resource "aws_iam_group" "devops_group" {
 resource "aws_iam_user" "bob" {
   name = "bob"
 }
-resource "aws_iam_user" "Kate" {
-  name = "Kate"
+resource "aws_iam_user" "kate" {
+  name = "kate"
 }
 
