bec3cc317a61bbf762f32d0a52aa7ab7816c9f19
Shukur91 <ssean6754@gmail.com>
2025-01-24 16:36:18 -0600

    fixed name

--- class1/main.tf
+++ class1/main.tf
@@ -8,7 +8,7 @@ resource "aws_iam_group_membership" "devops_group_membership" {
   name  = "devops-group-membership"
   group = aws_iam_group.devops_group.name
 
-  users = [ "bob", "kate"]
+  users =  [aws_iam_user.bob.name, aws_iam_user.kate.name]
 }
 
 
@@ -19,10 +19,10 @@ resource "aws_iam_user" "kate" {
   name = "Kate"
 }
 
-resource "aws_iam_group_policy_attachment" "devops_group_policy" {
-  group      = aws_iam_group.devops_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
+# resource "aws_iam_group_policy_attachment" "devops_group_policy" {
+#   group      = aws_iam_group.devops_group.name
+#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+# }
 
 # Upload an existing public key to AWS
 resource "aws_key_pair" "example_key" {
