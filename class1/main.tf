1d7975e7cbccde3e1160990939412a7c188a1a1e
Shukur91 <ssean6754@gmail.com>
2025-01-24 12:18:04 -0600

    changed

--- class1/main.tf
+++ class1/main.tf
@@ -8,10 +8,7 @@ resource "aws_iam_group_membership" "devops_group_membership" {
   name  = "devops-group-membership"
   group = aws_iam_group.devops_group.name
 
-  users = [
-    aws_iam_user.bob.name,
-    aws_iam_user.kate.name
-  ]
+  users = [ "bob", "kate"]
 }
 
 
