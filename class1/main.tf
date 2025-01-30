5a11df6953175f656b87e3630510371081607d98
Shukur91 <ssean6754@gmail.com>
2025-01-30 16:10:56 -0600

    name

--- class1/main.tf
+++ class1/main.tf
@@ -37,7 +37,7 @@ resource "aws_s3_bucket" "example" {
 }
 
 resource "aws_iam_group_membership" "devops_group_membership" {
-  name  = "devops-group-membership"
+  name  = "admin"
   group = "adminsssssss"
   users =  [aws_iam_user.bob.name, aws_iam_user.kate.name]
 }
