97a3992648b542348e7ee9cab721bb83f2ddfde2
Shukur91 <ssean6754@gmail.com>
2025-01-30 16:35:55 -0600

    changed

--- class1/main.tf
+++ class1/main.tf
@@ -1,6 +1,10 @@
-# Create IAM group
+# Create IAM groups
 resource "aws_iam_group" "devops_group" {
   name = "devops-group"
+}
+
+resource "aws_iam_group" "admin_group" {
+  name = "adminsssssss"
 }
 
 # Create IAM users
@@ -9,17 +13,24 @@ resource "aws_iam_user" "bob" {
 }
 
 resource "aws_iam_user" "kate" {
-  name = "Kate"
+  name = "kate"
 }
 
-# Add users to the IAM group
+# Add users to the DevOps group
 resource "aws_iam_group_membership" "devops_group_membership" {
   name  = "devops-group-membership"
   group = aws_iam_group.devops_group.name
 
-  users = [aws_iam_user.bob.name, aws_iam_user.Kate.name]
+  users = [aws_iam_user.bob.name, aws_iam_user.kate.name]
 }
 
+# Add users to the Admin group
+resource "aws_iam_group_membership" "admin_group_membership" {
+  name  = "admin-group-membership"
+  group = aws_iam_group.admin_group.name
+
+  users = [aws_iam_user.bob.name, aws_iam_user.kate.name]
+}
 
 # Upload an existing public SSH key to AWS
 resource "aws_key_pair" "example_key" {
@@ -30,15 +41,4 @@ resource "aws_key_pair" "example_key" {
 # Create an S3 bucket
 resource "aws_s3_bucket" "example" {
   bucket_prefix = "my-tf-test-bucket"
-}
-
-# OPTIONAL: Another IAM group and membership (if "adminsssssss" exists)
-resource "aws_iam_group" "admin_group" {
-  name = "adminsssssss"
-}
-
-resource "aws_iam_group_membership" "team" {
-  name  = "admin-group-membership"
-  group = "adminsssssss"
-  users = [aws_iam_user.bob.name, aws_iam_user.Kate.name]
 }
