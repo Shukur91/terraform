380303e3843a564f530a5a3788afb211bf4c67d0
Shukur91 <ssean6754@gmail.com>
2025-01-30 16:11:46 -0600

    team

--- class1/main.tf
+++ class1/main.tf
@@ -36,8 +36,8 @@ resource "aws_s3_bucket" "example" {
   bucket_prefix = "my-tf-test-bucket"
 }
 
-resource "aws_iam_group_membership" "devops_group_membership" {
-  name  = "admin"
+resource "aws_iam_group_membership" "team" {
+  name  = "devops-group-membership"
   group = "adminsssssss"
   users =  [aws_iam_user.bob.name, aws_iam_user.kate.name]
 }
