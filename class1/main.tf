cff37bf2adeeb4b6c386b074183eb2ddddf5ab41
Shukur91 <ssean6754@gmail.com>
2025-01-31 15:50:54 -0600

    changed

--- class1/main.tf
+++ class1/main.tf
@@ -1,12 +1,3 @@
-# Create IAM groups
-resource "aws_iam_group" "devops_group" {
-  name = "devops-group"
-}
-
-resource "aws_iam_group" "admin_group" {
-  name = "adminsssssss"
-}
-
 # Create IAM users
 resource "aws_iam_user" "bob" {
   name = "bob"
@@ -17,20 +8,20 @@ resource "aws_iam_user" "kate" {
 }
 
 # Add users to the DevOps group
-resource "aws_iam_group_membership" "devops_group_membership" {
-  name  = "devops-group-membership"
-  group = aws_iam_group.devops_group.name
+# resource "aws_iam_group_membership" "devops_group_membership" {
+#   name  = "devops-group-membership"
+#   group = aws_iam_group.devops_group.name
 
-  users = [aws_iam_user.bob.name, aws_iam_user.kate.name]
-}
+#   users = [aws_iam_user.bob.name, aws_iam_user.kate.name]
+# }
 
-# Add users to the Admin group
-resource "aws_iam_group_membership" "admin_group_membership" {
-  name  = "admin-group-membership"
-  group = aws_iam_group.admin_group.name
+# # Add users to the Admin group
+# resource "aws_iam_group_membership" "admin_group_membership" {
+#   name  = "admin-group-membership"
+#   group = aws_iam_group.admin_group.name
 
-  users = [aws_iam_user.bob.name, aws_iam_user.kate.name]
-}
+#   users = [aws_iam_user.bob.name, aws_iam_user.kate.name]
+# }
 
 # Upload an existing public SSH key to AWS
 resource "aws_key_pair" "example_key" {
@@ -38,7 +29,7 @@ resource "aws_key_pair" "example_key" {
   public_key = file("~/.ssh/id_rsa.pub") # Path to your public key file
 }
 
-# Create an S3 bucket
-resource "aws_s3_bucket" "example" {
-  bucket_prefix = "my-tf-test-bucket"
-}
+# # Create an S3 bucket
+# resource "aws_s3_bucket" "example" {
+#   bucket_prefix = "my-tf-test-bucket"
+# }
